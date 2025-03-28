2066f05c26236e7730fdbe90d7bf99915853729d
Tanishq Chhabra <tanishqchhabra98@gmail.com>
2025-03-28 13:06:48 +0530

    Solution to Factorial Range

---
+++ Factorial Range@@67de00956ac3fec9afb90e10/code.c
@@ -0,0 +1,13 @@
+// Your code here...
+int factorialRange(int start, int end){
+    if ((start==0 && end==0) || start==1 || end==1){
+        return 1;
+    }
+    else{
+        for(i=start; i<=end; i++){
+            fact=1;
+            fact *= i;
+        }
+        return fact;
+    }
+}
